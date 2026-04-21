b1cb690d5135ddf4a88c80fe2707cd419b84e206
valen <valen@duck.com>
2026-04-21 17:52:26 -0300

    feat: agrega aclaración sobre devolver el hash

--- template/src/services/me.service.ts
+++ template/src/services/me.service.ts
@@ -8,5 +8,5 @@ import { obtenerUsuarioPorUuid, type Usuario, type UUID } from "../databases/use
 export function obtenerPerfil(uuid: UUID): Usuario {
   const user = obtenerUsuarioPorUuid(uuid);
   if (!user) throw new Error("Usuario no encontrado");
-  return user;
+  return user; // Tener en cuenta que en la vida real, uno no debería devolver la clave, ni aunque este hasheada
 }
